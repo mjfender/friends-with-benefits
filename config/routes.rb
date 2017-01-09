58eb465349045e577dff716738bd541b98b56561
Elizabeth Jacobs <elizabethjjacobs@gmail.com>
2017-01-09 17:45:34 -0500

    created sessions for user to login

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
 
   root 'users#new'
   # get '/signup', to: 'users#new', as: 'signup'
-  
+
   get '/users', to: 'users#index', as: 'users'
   get '/users/:id', to: 'users#show', as: 'user'
-
   post '/users', to: 'users#create'
+
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/sessions', to: 'sessions#create'
 
 end
