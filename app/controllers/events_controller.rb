dad68ef3906c55e6e652b4b4d794c3c249c1f31a
Matt Fender <mjfender@gmail.com>
2017-01-11 14:37:37 -0500

    added working new and show pages

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_each :set_user
+  before_action :set_user
 
   def new
     @need = Need.find(params[:need_id])
@@ -8,17 +8,20 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
+    @event.need_id = params[:need_id]
+    @need = Need.find(params[:need_id])
     if @event.save
       flash[:notice] = "Woo! You just made an event."
-      redirect_to @event
+      @event.populate
+      redirect_to need_event_path(@need, id: @event.id)
     else
       flash[:notice] = @event.errors.full_messages.join(" ")
-      redirect_to new_event_path
+      redirect_to new_need_event_path
     end
   end
 
   def show
-
+    @event = Event.find(params[:id])
   end
 
   def index
--- app/models/event_user.rb
+++ app/models/event_user.rb
@@ -1,4 +1,4 @@
-class EventUsers < ApplicationRecord
+class EventUser < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
