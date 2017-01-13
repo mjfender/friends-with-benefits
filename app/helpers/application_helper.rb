618b887e4ddc47cb189ef8c6f0e7be54980dbca2
Ashley Robinson <ashleyhowerobinson@gmail.com>
2017-01-13 11:35:01 -0500

    pictures!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def profile_pic(model)
+    picture = "<div class='avatar'>" +
+    link_to((image_tag model.avatar.url(:thumb), class: "thumbnail"), model) +
+    "</div>"
+    picture.html_safe
+  end
+
 end
+
